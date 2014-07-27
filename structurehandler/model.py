3bc045a56b3b19b8e40ffa39971f7b542d83031a
Morten Kjeldgaard <mok@bioxray.dk>
2014-07-27 20:01:54 +0200

    Move CRAtree class to separate module

--- structurehandler/model.py
+++ structurehandler/model.py
@@ -28,22 +28,3 @@ class Model:
     #.
 #.
 
-from .chain import Chain
-
-# The CRAtree object contains a hierachy of chain, residue and atom objects.
-
-class CRAtree:
-    def __init__(self, model):
-
-        self.chains = {}
-
-        for x in model.cids:
-
-            # Populate self.chains, an index into the residues array.
-
-            reslist = model.residues[model.residues['cid'] == x]
-            atmlist = model.atoms[model.atoms['cid'] == x]
-            self.chains[x] = Chain(x, reslist, atmlist)
-        #.
-    #.
-#
